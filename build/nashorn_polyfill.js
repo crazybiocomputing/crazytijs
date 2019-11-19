e899b48247de82dc01ebbf3e7701e5988e9ea9db
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2019-11-19 15:59:06 +0100

    Update

--- build/nashorn_polyfill.js
+++ build/nashorn_polyfill.js
@@ -9,13 +9,51 @@ var global = this;
 var window = this;
 var process = {env:{}};
 
-var console = {};
+/*
+ * Adding some console functions
+ */
+const console = {};
+console.assert = (assertion,msg) => {
+  if (!assertion) {
+    print('Assertion failed: ' + msg);
+  }
+}
 console.debug = print;
+
+/*
+ * 
+ */
 console.log = print;
-console.warn = print;
+
+/*
+ * 
+ */
+console.logAll = (args) => {
+  const sep = ' ';
+  let msg = '';
+  // Old way - Destructuring not implemented in Nashorn
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] === 'object') {
+      msg += JSON.stringify(args[i]) + sep;
+    }
+    else {
+      msg += args[i].toString() + sep;
+    }
+  }
+  print(msg);
+}
+
+
+
+console.warn = (msg) => {
+  print('⚠ '+ msg);
+}
 console.error = print;
+console.info = (msg) => {
+  print('ⓘ ' + msg);
+}
 
-
+const toArrayJS = (java_array) => Java.from(java_array);
 
 /*
  * Specific Nashorn code for IMAGEJ to Extend ImageWindow
@@ -101,6 +139,58 @@ function setOverlay(renderer,root) {
   imp.setWindow(myWin);
 
 }
+
+/**
+ * Polyfill for Array.findIndex
+ */
+ 
+// https://tc39.github.io/ecma262/#sec-array.prototype.findIndex
+if (!Array.prototype.findIndex) {
+  Object.defineProperty(Array.prototype, 'findIndex', {
+    value: function(predicate) {
+     // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return k.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return k;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return -1.
+      return -1;
+    },
+    configurable: true,
+    writable: true
+  });
+}
+
 
 
 /**
